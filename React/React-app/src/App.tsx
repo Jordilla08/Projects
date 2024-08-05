aba24e777ff1e71684866d9c145eed8c23231a2e
Jordan Kelly <jkyne6368@yahoo.com>
2024-08-05 14:25:10 -0400

    more training
    more react training

--- React/React-app/src/App.tsx
+++ React/React-app/src/App.tsx
@@ -1,18 +1,12 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = [
-    'New York',
-    'Los Angeles',
-    'Tokyo',
-    'London',
-    'Paris',
-]
 
-const handleSelectItem = (item: string) => {
-  console.log(item);
-}
-  return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} /></div>
+  return <div>
+    <Alert>
+      Hello <span> World</span>
+      </Alert>
+      </div>
 }
 
 export default App;
