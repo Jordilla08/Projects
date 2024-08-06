1c0be4aa3f12f7665d6a5f64c12a9e098817ee8a
Jordan Kelly <jkyne6368@yahoo.com>
2024-08-06 13:11:35 -0400

    Build Navbar

--- Gamehub/game-hub/src/App.tsx
+++ Gamehub/game-hub/src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react"
+import NavBar from "./components/NavBar"
 
 
 function App() {
@@ -6,7 +7,8 @@ function App() {
     base: `"nav" "main"`,
     lg: `"nav nav" "aside main"`
   }}>
-    <GridItem area='nav' bg='coral'>Nav</GridItem>
+    <GridItem area='nav'><NavBar /></GridItem>
+  
     <Show above="lg">
     <GridItem area='aside' bg='gold'>Aside</GridItem>
     </Show>
---
+++ Gamehub/game-hub/src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import { HStack, Image, Text } from '@chakra-ui/react'
+import logo from '../assets/logo.webp'
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <HStack>
+        <Image src={logo} boxSize='60px' />
+        <Text>NavBar</Text>
+    </HStack>
+  )
+}
+
+export default NavBar
