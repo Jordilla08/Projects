70e08f68fd25b4429119f54cda27a1a79928f210
Jordan Kelly <jkyne6368@yahoo.com>
2024-08-06 13:43:57 -0400

    Built color mode switch

--- Gamehub/game-hub/src/App.tsx
+++ Gamehub/game-hub/src/App.tsx
@@ -10,9 +10,9 @@ function App() {
     <GridItem area='nav'><NavBar /></GridItem>
   
     <Show above="lg">
-    <GridItem area='aside' bg='gold'>Aside</GridItem>
+    <GridItem area='aside'>Aside</GridItem>
     </Show>
-    <GridItem area='main' bg='dodgerblue'>Main</GridItem>
+    <GridItem area='main'>Main</GridItem>
   </Grid>
 
 }
