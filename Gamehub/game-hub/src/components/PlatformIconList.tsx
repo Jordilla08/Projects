59d62d190f322ce178b39ba15224468cf66227d6
Jordan Kelly <jkyne6368@yahoo.com>
2024-08-06 17:50:35 -0400

    Displaying Platform Icons

---
+++ Gamehub/game-hub/src/components/PlatformIconList.tsx
@@ -0,0 +1,34 @@
+import {FaWindows, FaXbox, FaPlaystation, FaApple, FaLinux, FaAndroid} from 'react-icons/fa'
+import { MdPhoneIphone } from 'react-icons/md'
+import { SiNintendo } from 'react-icons/si'
+import { BsGlobe } from 'react-icons/bs'
+import { Platform } from '../Hooks/useGames'
+import { HStack, Icon, Text } from '@chakra-ui/react'
+import { IconType } from 'react-icons'
+
+
+interface Props {
+    platforms: Platform[]
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+    const iconMap: {[key: string]: IconType} = {
+        pc: FaWindows,
+        xbox: FaXbox,
+        playstation: FaPlaystation,
+        ios: MdPhoneIphone,
+        mac: FaApple,
+        linux: FaLinux,
+        android: FaAndroid,
+        nintendo: SiNintendo,
+        web: BsGlobe
+    }
+  return (
+    <HStack marginY={'10px'}>
+    {platforms.map((platform) => 
+        <Icon as={iconMap[platform.slug]} color='gray.500' />)}
+    </HStack>
+  )
+}
+
+export default PlatformIconList
