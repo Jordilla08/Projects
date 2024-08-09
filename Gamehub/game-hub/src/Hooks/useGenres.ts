1ffead36bceb93929d6af3305e413f651f329ac6
Jordan Kelly <jkyne6368@yahoo.com>
2024-08-09 10:03:40 -0400

    Display genres
    also removed fixed width from cards to prevent collapse

--- Gamehub/game-hub/src/Hooks/useGenres.ts
+++ Gamehub/game-hub/src/Hooks/useGenres.ts
@@ -4,6 +4,7 @@ import useData from './useData';
 export interface Genre {
     id: number;
     name: string;
+    background_image: string;
 }
 
 
--- Gamehub/game-hub/src/components/GameGrid.tsx
+++ Gamehub/game-hub/src/components/GameGrid.tsx
@@ -13,7 +13,7 @@ const GameGrid = () => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        columns={{ base: 1, sm: 1, md: 2, lg: 3, xl: 4, '2xl': 5 }}
         padding="10px"
         spacing={3}
       >
