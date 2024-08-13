ce3c536bd8224afa762fa3629f2e3187f3230a66
Jordan Kelly <jkyne6368@yahoo.com>
2024-08-13 08:27:07 -0400

    filter games by platform

--- Gamehub/game-hub/src/App.tsx
+++ Gamehub/game-hub/src/App.tsx
@@ -5,10 +5,13 @@ import GenreList from "./components/GenreList"
 import { useState } from "react"
 import { Genre } from "./Hooks/useGenres"
 import PlatformSelector from "./components/PlatformSelector"
+import { Platform } from "./Hooks/useGames"
 
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null);
+
   return (
     <Grid
       templateAreas={{
@@ -29,8 +32,8 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <PlatformSelector />
-        <GameGrid selectedGenre={selectedGenre} />
+        <PlatformSelector selectedPlatform={selectedPlatform} onSelectPlatform={(platform) => setSelectedPlatform(platform)} />
+        <GameGrid selectedPlatform={selectedPlatform} selectedGenre={selectedGenre} />
       </GridItem>
     </Grid>
   );
