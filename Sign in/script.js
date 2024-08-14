d04f211ecab3fcdd6f5b4f4d1724b18980a81e89
Jordan Kelly <jkyne6368@yahoo.com>
2024-08-14 16:48:17 -0400

    refactored code

--- Sign in/script.js
+++ Sign in/script.js
@@ -1,11 +1,11 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');  // Get the register button
-const loginBtn = document.getElementById('login');  // Get the login button
+const container = document.getElementById("container");
+const registerBtn = document.getElementById("register"); // Get the register button
+const loginBtn = document.getElementById("login"); // Get the login button
 
-registerBtn.addEventListener('click', () => {
+registerBtn.addEventListener("click", () => {
   container.classList.add("active");
 });
 
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-  });
+loginBtn.addEventListener("click", () => {
+  container.classList.remove("active");
+});
