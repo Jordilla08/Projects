670c796cb44b61aa2f7dde81a20dacebb0ef50df
Jordan Kelly <jkyne6368@yahoo.com>
2024-08-13 20:27:03 -0400

    added emojis as ratings

---
+++ Gamehub/game-hub/src/components/Emoji.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import meh from '../assets/meh.webp';
+import thumbsUp from '../assets/thumbs-up.webp';
+import bullsEye from '../assets/bulls-eye.webp';
+import { Image, ImageProps } from '@chakra-ui/react';
+
+interface Props {
+    rating: number;
+}
+
+const Emoji = ({ rating }: Props) => {
+    if (rating <3) return null;
+
+const emojiMap: {[key: number]: ImageProps} = {
+    3: { src: meh, alt: 'meh', boxSize: '25px' },
+    4: { src: thumbsUp, alt: 'recommended', boxSize: '25px' },
+    5: { src: bullsEye, alt: 'exceptional', boxSize: '35px' },
+}
+
+  return (
+    <Image {...emojiMap[rating]} marginTop={2} />
+
+  )
+}
+
+export default Emoji
--- Gamehub/game-hub/src/components/GameCard.tsx
+++ Gamehub/game-hub/src/components/GameCard.tsx
@@ -4,6 +4,7 @@ import { Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react'
 import PlatformIconList from './PlatformIconList'
 import CriticScore from './CriticScore'
 import getCropppedImageUrl from '../services/image-url'
+import Emoji from './Emoji'
 
 
 interface Props {
@@ -19,7 +20,7 @@ const GameCard = ({ game }: Props) => {
               <PlatformIconList platforms={game.parent_platforms.map((p) => p.platform)} />
               <CriticScore score={game.metacritic} />
             </HStack>
-            <Heading fontSize='2xl'>{game.name}</Heading>
+            <Heading fontSize='2xl'>{game.name}<Emoji rating={game.rating_top} /></Heading>
         </CardBody>
     </Card>
   )
